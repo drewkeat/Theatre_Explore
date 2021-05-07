0760f39904f80cf1315e8d47de7374975a9cfc8f
Drew Keat <thedrewkeat@gmail.com>
2021-05-06 22:57:22 -0600

    Adjusted file structure to clarify environment, execution point, console, and class libraries

--- lib/Theatre_Explore/cli.rb
+++ lib/Theatre_Explore/cli.rb
@@ -1,7 +1,12 @@
 class Theatre_Explore::CLI
     def call
+        puts "Welcome to Theatre Explore!"
         puts "\nHow would you like to search?"
 
+        display_options
+    end
+
+    def display_options
         puts "
         1) Productions by year
         2) Productions by venue
