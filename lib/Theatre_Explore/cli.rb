f7a0d2fb474a5bef8cbadc4019d1fc59d7c47e2b
Drew Keat <thedrewkeat@gmail.com>
2021-05-06 22:45:21 -0600

    ditto

---
+++ lib/Theatre_Explore/cli.rb
@@ -0,0 +1,11 @@
+class Theatre_Explore::CLI
+    def call
+        puts "\nHow would you like to search?"
+
+        puts "
+        1) Productions by year
+        2) Productions by venue
+        3) Productions by show title"
+        puts "\nSelect 1-3"
+    end
+end
