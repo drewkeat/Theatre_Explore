e78b8702a07040df5772cb254fab69fc5ac00c13
Drew Keat <thedrewkeat@gmail.com>
2021-05-21 09:40:41 -0600

    Update filestructure for publishing

---
+++ lib/Theatre_Explore.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "Theatre_Explore/version"
+require_relative "Theatre_Explore/cli"
+require_relative "Theatre_Explore/Year"
+require_relative "Theatre_Explore/Show"
+require_relative "Theatre_Explore/Production"
+require_relative "Theatre_Explore/Scraper"
+
+require 'pry'
+require 'nokogiri'
+require 'mechanize'
+
